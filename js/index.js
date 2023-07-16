bdf92d2870ba57b9004804c5a4c6140f5b2e8784
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2023-07-16 21:36:57 +0300

    dgtdrf

---
+++ js/index.js
@@ -0,0 +1,108 @@
+// ----------------task 1------------------
+
+// const stars = 9;
+// let price;
+
+// if (stars === 1) {
+//     price = 20;
+// } else if (stars === 2) {
+//     price = 30;
+// } else if (stars === 3) {
+//     price = 50;
+// } else if (stars === 4) {
+//     price = 70;
+// } else if (stars === 5) {
+//     price = 120;
+// } else {
+//     console.log(" There are not such count of stars");
+// }
+
+// console.log(price);
+
+// switch (stars) {
+//     case 1:
+//         price = 20;
+//         break;
+//     case 2:
+//         price = 30;
+//         break;
+//      case 3:
+//         price = 50;
+//         break;
+//     case 4:
+//         price = 70;
+//         break;
+//     case 5:
+//         price = 120;
+//         break;
+    
+//     default : console.log(" There are not such count of stars");
+// }
+// console.log(price)
+
+
+// const stars = 6;
+//  let price;
+
+// if (stars === 1 || stars === 2) {
+//     price = 20;
+//  } else if (stars === 3 || stars === 4) {
+//    price = 30;
+//  } else if (stars === 5) {
+//    price = 120;
+//  } else {
+//      console.log(" There are not such count of stars");
+//  }
+
+//  console.log(price);
+
+
+
+// ------------------------task 2-----------------------------
+// const option = "fhh";
+// let message = "";
+
+// switch (option) {
+//     case 1:
+//        message = "You can take tour order in store"
+//         break;
+    
+//     case 2:
+//        message = "We will deliver yout order by courier"
+//         break;
+    
+//     case 3:
+//        message = "We will deliver yout order by post"
+//         break;
+
+//     default: message = "Our manager will call you"
+// }
+
+// console.log(message);
+
+
+// -----------------------task 3------------------------------
+
+//  for (let i = 100; i >= 0; i -= 5) {
+//     console.log(i);
+// }
+
+// console.log("no more");
+
+
+// ------------task 4-----------------------
+
+// const emploee = 6;
+// let totalSalary = 0;
+// const minSalary = 500;
+// const maxSalary = 5000;
+
+// for (let i = 1; i < 7; i += 1) {
+//     const salary = Math.round(Math.random() * (maxSalary - minSalary) + minSalary);
+//     console.log(`Salary emploee number ${i} - ${salary}`);
+//     totalSalary += salary;
+
+// }
+// console.log("total ", totalSalary);
+
+
